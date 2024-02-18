41bdac7c5218c87ddf5c98e0fb5331dfc176b94a
AYAH HAYMOUR <aahaymour20@cit.just.edu.jo>
2024-02-19 00:29:34 +0300

    ...

--- IT Roadmap.Infra/Common/DbContext.cs
+++ IT Roadmap.Infra/Common/DbContext.cs
@@ -13,34 +13,44 @@ namespace IT_Roadmap.Infra.Common
 {
     public class DbContext : IDbContext
     {
+
         private DbConnection _connection;
         private readonly IConfiguration _configuration;
+
         public DbContext(IConfiguration configuration)
         {
             _configuration = configuration;
         }
 
+
+        // 1- open 
+        // 2- close -- open 
+        // 3- no connection --> creat connection --> open 
         public DbConnection Connection
         {
             get
             {
+
                 if (_connection == null)
                 {
-                    _connection = new OracleConnection
-                    (_configuration["ConnectionStrings: DBConnectionString"]);
-                    _connection.Open();
-                }
-                else if (_connection.State !=
-                ConnectionState.Open)
-                {
+                    //create 
+                    _connection = new OracleConnection(_configuration["ConnectionStrings:DefaultConnection"]);
+                    //open 
                     _connection.Open();
                 }
 
+
+                // close -->open 
+                else if (_connection.State != ConnectionState.Open)
+                {
+                    _connection.Open();
+                }
+                // open --> return connection 
                 return _connection;
 
             }
-
-
         }
+
     }
+
 }
